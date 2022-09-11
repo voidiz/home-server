5b152ba9cbf2bbe620439f54e3c4aa2f0024bcb2
voidiz <voidqt@hotmail.com>
2022-09-11 18:11:39 +0200

    remove unused mdns setup from `setup.sh`
    replaced by tailscale + pihole

--- setup.sh
+++ setup.sh
@@ -24,22 +24,6 @@ function install_docker() {
     systemctl enable docker --now
 }
 
-function setup_mdns() {
-    [ ! -x "$(command -v mdns-publish-cname)" ] && {
-        echo "Installing mdns-publisher"
-        pip3 install mdns-publisher
-    }
-
-    [ ! -f "/etc/systemd/system/mdns-publisher.service" ] &&
-        ln -s $PWD/services/mdns-publisher/mdns-publisher.service /etc/systemd/system/mdns-publisher.service
-
-    [ ! -f "/usr/local/bin/mdns-publisher.sh" ] &&
-        ln -s $PWD/services/mdns-publisher/mdns-publisher.sh /usr/local/bin/mdns-publisher.sh
-
-    systemctl daemon-reload
-    systemctl enable --now mdns-publisher.service
-}
-
 function setup_tailscale() {
     [ ! -x "$(command -v tailscale)" ] && {
         curl -fsSL https://tailscale.com/install.sh | sh
@@ -49,6 +33,5 @@ function setup_tailscale() {
     tailscale up --accept-dns=false
 }
 
-setup_mdns
 install_docker
 setup_tailscale
